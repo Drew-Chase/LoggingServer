f555d56cf3903b9a93af1f9830a7d0a173f03683
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-05-08 09:20:01 -0400

    Expanded Logger.js with additional log types and server functionality
    The Logger.js file has been significantly expanded and improved. It now includes different log types such as DEBUG, INFO, WARNING, ERROR, and FATAL. In addition, the Logger now has the ability to send log messages to a specified server.

--- LoggingServer/wwwroot/js/Logger.js
+++ LoggingServer/wwwroot/js/Logger.js
@@ -1,7 +1,91 @@
-﻿export class Logging {
-    constructor() {
-        this.log = (message) => {
-            console.log(message);
-        }
+﻿const LogTypes = {
+    DEBUG: 'DEBUG',
+    INFO: 'INFO',
+    ERROR: 'ERROR',
+    WARNING: 'WARNING',
+    FATAL: 'FATAL',
+};
+
+export class Logger {
+
+
+    /**
+     * @param {string} server - The server url or ipaddress to connect to
+     */
+    constructor(server) {
+        this.server = server;
+    }
+
+    /**
+     * Logs a debug message and sends it to the log server.
+     *
+     * @param {string} message - The debug message to log.
+     * @return {Promise<void>} A Promise that resolves when the message is logged and sent to the server.
+     */
+    async debug(message) {
+        console.debug(message);
+        await this.sendLog(message, LogTypes.DEBUG);
+    }
+
+    /**
+     * Logs the provided message with the INFO log type.
+     *
+     * @param {string} message - The message to log.
+     *
+     * @return {Promise<void>} - A promise that resolves when the log is sent.
+     */
+    async info(message) {
+        console.log(message);
+        await this.sendLog(message, LogTypes.INFO);
+    }
+
+    /**
+     * Logs a warning message and sends it to the server.
+     *
+     * @param {string} message - The warning message.
+     * @return {Promise} - A promise that resolves when the warning message is logged and sent.
+     */
+    async warning(message) {
+        console.warn(message);
+        await this.sendLog(message, LogTypes.WARNING);
+    }
+
+    /**
+     * Logs an error message and sends it to the server.
+     *
+     * @param {string} message - The error message to be logged.
+     *
+     * @return {Promise<void>} - A promise that resolves when the error message has been sent to the server.
+     */
+    async error(message) {
+        console.error(message);
+        await this.sendLog(message, LogTypes.ERROR);
+    }
+
+
+    /**
+     * Logs a fatal error message and sends it to the log server asynchronously.
+     *
+     * @param {string} message - The fatal error message to log.
+     * @return {Promise<void>} - A promise that resolves when the log is sent.
+     */
+    async fatal(message) {
+        console.error(message);
+        await this.sendLog(message, LogTypes.FATAL);
+    }
+
+    /**
+     * Sends a log message to the server
+     * @param {string} message - The message to log
+     * @param {LogTypes} type - The type of log message
+     */
+    async sendLog(message, type) {
+        await fetch(`${this.server}/${window.location.hostname}/${type}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({message: message})
+        });
     }
 }
